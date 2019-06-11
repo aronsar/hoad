b63edabe40f37728be4ac4fdb32fde7f22522699
Aron Sarmasi <aronsar@gmail.com>
2019-06-11 10:38:43 -0700

    setting up hanabi learning environment

---
+++ run_experiment.py
@@ -0,0 +1,16 @@
+# This script is meant as an end to end, data creator, trainer, and evaluater.
+# It is set up so that the tasks within can easily be done manually as well.
+
+#import statements
+
+def main():
+    #parse arguments
+    
+    
+    #create/load data
+
+
+    #train model/load model
+
+
+    #load model, evaluate model
