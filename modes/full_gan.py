36df33900f5d27c1fda6d6e3f796fec89958d127
Aron Sarmasi <aronsar@gmail.com>
2019-07-25 06:24:36 -0700

    got code working up until when DataGenerator is called in full_gan.py

--- modes/full_gan.py
+++ modes/full_gan.py
@@ -1,11 +1,13 @@
 #This mode is for the full model with all the bells and whistles.
 
+import gin
 import tensorflow as tf
 from keras.layers import Input
 from keras.models import Model
-import network_elements as ne
+from modes import network_elements as ne
+import datagenerator as dg
 
-@gin_configurable
+@gin.configurable
 def build_model(cfg={}):
     adhoc_games = Input(shape=(658,))
     input_obs = Input(shape=())
@@ -16,3 +18,30 @@ def build_model(cfg={}):
     same_agent = ne.discriminator(input_obs, agent_act, input_act, cfg)
 
     return Model(inputs=[adhoc_games, input_obs, input_act], outputs=same_agent)
+    
+#TODO: this stuff below
+@gin.configurable
+class DataGenerator(dg.BaseDataGenerator):
+    @gin.configurable
+    def __init__(self, raw_data, cfg={}):
+        # Set Before calling init of base class
+        self.batch_size = cfg["batch_size"]
+        self.shuffle = cfg["shuffle"]
+        self.obs_dim = cfg["obs_dim"]
+        self.act_dim = cfg["act_dim"]
+        self.verbose = cfg["verbose"]
+
+        super().__init__(raw_data)
+
+    """
+    FYI: This is an example of overriding methods in BaseDataGenerator
+    """
+    def batch_sampler(self, index):        
+        """
+        batch_sampler(self)
+        Args: None
+        Usage:
+            Specificies the logic of sampling the data. This is an implementation of random sampling with repitive possibility
+        """
+
+        return np.random.randint(low=0, high=self.epoch_len, size=self.batch_size)
