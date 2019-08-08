87ed718a96cfd9496d5c80f6dcb2fe33a1fea557
Timothy Zhang <tsme4wk@gmail.com>
2019-08-08 14:00:43 -0700

    added function that converts an int back to a bin list

--- utils/binary_list_to_int.py
+++ utils/binary_list_to_int.py
@@ -7,3 +7,14 @@ def convert(bin_list):
         - Converted integer.
     """
     return sum(x << i for i, x in enumerate(reversed(bin_list)))
+
+def revert(x, length):
+    """ Revert an integer back to a binary list.
+    Arguments:
+        - x: int
+            An integer that is to be reverted back to a binary list.
+        - length: int
+            Total length of the binary list that will be returned.
+            If x >= (2^length), there will be no padding.
+    """
+    return [int(i) for i in list(format(x, '0%db' % length))]
