5227e21a0611e9bd80490a3513454a73d357cc7d
Skyler Bala <skyler.bala@gmail.com>
2019-08-07 20:35:43 -0700

    add makefile stuff

---
+++ hanabi_env/hanabi_lib/CMakeFiles/hanabi.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hanabi.dir/hanabi_card.cc.o"
+  "CMakeFiles/hanabi.dir/hanabi_game.cc.o"
+  "CMakeFiles/hanabi.dir/hanabi_hand.cc.o"
+  "CMakeFiles/hanabi.dir/hanabi_history_item.cc.o"
+  "CMakeFiles/hanabi.dir/hanabi_move.cc.o"
+  "CMakeFiles/hanabi.dir/hanabi_observation.cc.o"
+  "CMakeFiles/hanabi.dir/hanabi_state.cc.o"
+  "CMakeFiles/hanabi.dir/util.cc.o"
+  "CMakeFiles/hanabi.dir/canonical_encoders.cc.o"
+  "libhanabi.pdb"
+  "libhanabi.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hanabi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
