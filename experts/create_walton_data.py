07a32e92b735a94980d1eb9aa25ead28e9304a4f
Timothy Zhang <tsme4wk@gmail.com>
2019-08-21 19:00:18 -0700

    fixed redundant seed from a mistake during merging

--- experts/create_walton_data.py
+++ experts/create_walton_data.py
@@ -201,9 +201,6 @@ def create_pkl_data(args, csv_data):
 
 def act_based_pipeline(args):
     # Sort Params
-    # seed = 1
-    seed = random.randint(0, 2**31-1)
-    print('seed:', seed)
     csv_filename, pkl_filename, jar_filename = create_data_filenames(args)
 
     # Generate game seed (Might not be needed TBH)
