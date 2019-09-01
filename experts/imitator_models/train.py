f93c8008e768b337c7d1b6d4a2b288702782d6a3
Timothy Zhang <tsme4wk@gmail.com>
2019-09-01 12:24:42 -0700

    added training resuming

--- experts/imitator_models/train.py
+++ experts/imitator_models/train.py
@@ -10,66 +10,6 @@ import h5py_cache
 
 # multiprocessing.set_start_method('spawn', force=True)
 
-def new(args):
-    """ Creates new model since the model directory does not exist.
-    """
-
-    print('{} does not exist or is empty. Creating new model.'.format(args.p))
-
-    PATH_DIR_CKPT = os.path.join(args.m, 'ckpts')
-
-    n_epoch = 50
-    hypers = {'lr': 0.00015,
-              'batch_size': 512,
-              'hl_activations': [ReLU, ReLU, ReLU],
-              'hl_sizes': [1024, 512, 256],
-              'decay': 0.,
-              'bNorm': False,
-              'dropout': True,
-              'regularizer': None}
-
-    # checking input data format.
-    if args.p.split('.')[-1] in ['hdf5', 'HDF5']:
-        f = h5py_cache.File(p, 'r', chunk_cache_mem_size=1*1024**3, swmr=True)
-        gen_tr = Gen4h5(f['X_tr'], f['Y_tr'], hypers['batch_size'], False)
-        gen_va = Gen4h5(f['X_va'], f['Y_va'], hypers['batch_size'], False)
-    else:
-        X, Y, mask = CV(args.p)
-        gen_tr = DataGenerator(X[mask], Y[mask], hypers['batch_size'])
-        gen_va = DataGenerator(X[~mask], Y[~mask], 1000)
-
-
-    os.makedirs(PATH_DIR_CKPT, exist_ok=True)
-
-    # Callbacks: save best & latest models.
-    callbacks = [
-        ModelCheckpoint(
-            os.path.join(args.m, 'best.h5'), monitor='val_loss', verbose=1,
-            save_best_only=True,
-            save_weights_only=True, mode='auto', period=1
-        ),
-        ModelCheckpoint(
-            os.path.join(PATH_DIR_CKPT, '{epoch:02d}-{val_accuracy:.2f}.h5'),
-            monitor='val_loss', verbose=1, save_best_only=False,
-            save_weights_only=True, mode='auto', period=1
-        ),
-        CSVLogger(os.path.join(args.m, 'training.log'), append=True)
-        ]
-
-    m = Mlp(
-        io_sizes=(glb.SIZE_OBS_VEC, glb.SIZE_ACT_VEC),
-        out_activation=Softmax, loss='categorical_crossentropy',
-        metrics=['accuracy'], **hypers, verbose=1)
-    m.construct_model()
-    m.train_model(
-        gen_tr, gen_va, n_epoch=n_epoch, callbacks=callbacks,
-        verbose=True, workers=args.w, use_mp=True, max_q_size=args.q)
-
-    with open('./ckpts/rainbow_history_{}.pkl'.format(n_epoch), 'wb') as f:
-        pickle.dump(m.hist.history, f)
-
-    m.model.save('./ckpts/rainbow_model_{}.h5'.format(n_epoch)) # TODO: make it userdefined
-
 def model_exists(path):
     """ Check if model exists or is corrupted.
 
@@ -131,12 +71,71 @@ def model_exists(path):
         return True
 
 def main(args):
-    if model_exists(args.m):
+    # run this first to avoid failing after huge overhead
+    model_ok = model_exists(args.m)
+
+    PATH_DIR_CKPT = os.path.join(args.m, 'ckpts')
+
+    n_epoch = 50
+    hypers = {'lr': 0.00015,
+              'batch_size': 512,
+              'hl_activations': [ReLU, ReLU, ReLU],
+              'hl_sizes': [1024, 512, 256],
+              'decay': 0.,
+              'bNorm': False,
+              'dropout': True,
+              'regularizer': None}
+
+    # checking input data format.
+    if args.p.split('.')[-1] in ['hdf5', 'HDF5']:
+        f = h5py_cache.File(p, 'r', chunk_cache_mem_size=1*1024**3, swmr=True)
+        gen_tr = Gen4h5(f['X_tr'], f['Y_tr'], hypers['batch_size'], False)
+        gen_va = Gen4h5(f['X_va'], f['Y_va'], hypers['batch_size'], False)
+    else:
+        X, Y, mask = CV(args.p)
+        gen_tr = DataGenerator(X[mask], Y[mask], hypers['batch_size'])
+        gen_va = DataGenerator(X[~mask], Y[~mask], 1000)
+
+
+    os.makedirs(PATH_DIR_CKPT, exist_ok=True)
+
+    # Callbacks: save best & latest models.
+    callbacks = [
+        ModelCheckpoint(
+            os.path.join(args.m, 'best.h5'), monitor='val_loss', verbose=1,
+            save_best_only=True,
+            save_weights_only=True, mode='auto', period=1
+        ),
+        ModelCheckpoint(
+            os.path.join(PATH_DIR_CKPT, '{epoch:02d}-{val_accuracy:.2f}.h5'),
+            monitor='val_loss', verbose=1, save_best_only=False,
+            save_weights_only=True, mode='auto', period=1
+        ),
+        CSVLogger(os.path.join(args.m, 'training.log'), append=True)
+        ]
+
+    m = Mlp(
+        io_sizes=(glb.SIZE_OBS_VEC, glb.SIZE_ACT_VEC),
+        out_activation=Softmax, loss='categorical_crossentropy',
+        metrics=['accuracy'], **hypers, verbose=1)
+
+
+    if model_ok:
         # continue from previously saved
-        pass
+        msg = "Saved model found. Resuming training".format(args.p)
+        print(msg)
+        saved_h5 = os.path.join(args.m, 'best.h5') # FIXME: TAKE LATEST
+        m.construct_model(saved_h5, weights_only=True)
     else:
         # create new model
-        new(args)
+        msg = "{} doesn't exist or is empty. Creating new model.".format(args.p)
+        print(msg)
+        os.makedirs(PATH_DIR_CKPT, exist_ok=True)
+        m.construct_model()
+
+    m.train_model(
+        gen_tr, gen_va, n_epoch=n_epoch, callbacks=callbacks,
+        verbose=True, workers=args.w, use_mp=True, max_q_size=args.q)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
