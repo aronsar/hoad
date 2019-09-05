ed95d2c50d915c0ed190b9e92dab7466fb5cf656
Timothy Zhang <tsme4wk@gmail.com>
2019-09-04 19:23:45 -0700

    removed using screen & fixed path

--- experts/imitator_models/scripts/srvr/train.sh
+++ experts/imitator_models/scripts/srvr/train.sh
@@ -1,2 +1,1 @@
-screen -S train
-python3 /tmp/ganabi/experts/imitator_models/train.py --p tmp/$1 --m ~/saved_models --epochs 50
+python3 /tmp/ganabi/experts/imitator_models/train.py --p /tmp/$1 --m ~/saved_models --epochs 50
