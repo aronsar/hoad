66f47ca22d442b76e01b477ed8ae855d172b4fd4
huyenpham2995 <thanhhuyen2995@gmail.com>
2019-10-24 20:34:50 -0700

    data loader successfully generatr arff file

--- two_stage_transfer.py
+++ two_stage_transfer.py
@@ -8,11 +8,14 @@ import os
 import gin, os
 import random, pickle
 import numpy as np
+import weka.core.jvm as jvm
 from utils import parse_args
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import KFold
 from sklearn.metrics import accuracy_score
 
+jvm.start()
+from weka.classifiers import Classifier
 '''
 DATA LOADER FOR TWO STAGE TRANSFER
 '''
@@ -33,13 +36,12 @@ class DataLoader(object):
         self.get_source_data()
 
     def get_target_data(self):
-        target_agent_dir = random.choice(self.all_agents_datadir)
+        target_agent_dir = "quux_blindbot_data_2_500000"
         target_agent_name = "_".join(target_agent_dir.split("_")[:-3])
-        data = self.__get_25k_data(os.path.join(self.datapath,target_agent_dir))
         print("Getting target data for ", target_agent_name)
-        #self.target[target_agent_name] = data[:10]
-        self.target[target_agent_name] = self.__format_data(data[:10])
-        #print(self.target[target_agent_name])
+        data = self.__get_25k_data(os.path.join(self.datapath,target_agent_dir))
+        
+        self.target[target_agent_name] = self.write_data_to_arff(data[:10], target_agent_name, "target")
    
     def get_source_data(self):
         target_agent = list(self.target.keys())[0]
@@ -49,7 +51,7 @@ class DataLoader(object):
             if agent_name != target_agent:
                 print("Getting source data for ", agent_name)
                 data = self.__get_25k_data(os.path.join(self.datapath, agent_dir))
-                self.source[agent_name] = self.__format_data(data[:10])
+                self.source[agent_name] = self.write_data_to_arff(data[:100], agent_name, "source")
 
     def __get_all_agents(self):
         self.all_agents_datadir = [name for name in os.listdir(self.datapath)]
@@ -61,24 +63,39 @@ class DataLoader(object):
         path_to_file = os.path.join(first_dir, file_name)
         return pickle.load(open(path_to_file, "rb"), encoding='latin1')
 
-    def __format_data(self, games):
-        #print(games)
-        '''
-        Input data is a list of all games of 1 agent:
-            For each game:
-                + A list contains 2 list:  obs and acts of the whole game
-                    + In obs list: [12345, 67890,...]
-                    + In acts list: [[0,0,0,1,0,0],[1,0,0,0,0],...]
-        Output data format is a list with 2 lists:
-            + List 1: Obs of all games combined in 1 list
-            + List 2: Acts of all games combined in 1 list
-        '''
-        obs = games[0][0]
-        acts = games[0][1]
-        for i in range (1, len(games)):
-            obs += games[i][0]
-            acts += games[i][1]
-        return [obs, acts]
+    def write_data_to_arff(self, games, agent_name, datapath):
+        filename = os.path.join(datapath, agent_name + ".arff")
+        header = """@RELATION ObsActs
+@ATTRIBUTE obs NUMERIC
+@ATTRIBUTE agentName STRING
+@ATTRIBUTE weight NUMERIC
+@ATTRIBUTE class {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19}
+        
+@DATA
+"""
+        weight = 1
+
+        with open(filename,"w") as arff_file:
+            arff_file.write(header)
+            for game in games:
+                obs = game[0]
+                acts = game[1]
+                for step in range(len(obs)):
+                    arff_file.write("%d,%s,%d,%d\n" % (obs[step], agent_name, weight, self.bool_to_int(acts[step])))
+
+    def int_to_bool(self, intvec):
+        boolvec = np.array([])
+        for num in np.array(intvec):
+            temp=np.array(list('{0:b}'.format(num)), dtype=int)
+            #print (len(temp))
+            if(len(boolvec)==0):
+                boolvec=[np.pad(temp, ((658-len(temp)),0), 'constant', constant_values=(0,0 ))]
+            else:
+                boolvec = np.append(boolvec,[np.pad(temp, ((658-len(temp)),0), 'constant', constant_values=(0,0 ))],axis=0)
+        return boolvec
+
+    def bool_to_int(self, onehot):
+        return np.argmax(onehot)
 
 '''
 TwoStageTransfer (T, S, m, k, b)
@@ -99,7 +116,7 @@ class TwoStageTransfer:
             boosting_iter = 5,
             fold = 10,
             max_source_dataset = 1,
-            model = DecisionTreeClassifier()):
+            model = ""):
         '''
         - target is target data set (10 adhoc games from same agent)
         - S is set of source data sets S = {S_1, S_2, ..., S_n} (games from previously observed teammates,
@@ -116,23 +133,6 @@ class TwoStageTransfer:
         self.boosting_iter = boosting_iter
         self.fold = fold
         self.max_source_dataset = max_source_dataset
-
-    def int_to_bool(self, intvec):
-        boolvec = np.array([])
-        for num in np.array(intvec):
-            temp=np.array(list('{0:b}'.format(num)), dtype=int)
-            #print (len(temp))
-            if(len(boolvec)==0):
-                boolvec=[np.pad(temp, ((658-len(temp)),0), 'constant', constant_values=(0,0 ))]
-            else:
-                boolvec = np.append(boolvec,[np.pad(temp, ((658-len(temp)),0), 'constant', constant_values=(0,0 ))],axis=0)
-        return boolvec
-
-    def bool_to_int(self, onehot):
-        intvec = np.array([])
-        for num in np.array(onehot):
-            intvec = np.append(intvec, np.argmax(num))
-        return intvec
 
     def calculate_optimal_weight(self, target, w_source, source, boosting_iter, fold, err):
         '''
@@ -152,21 +152,21 @@ class TwoStageTransfer:
             weights.append (weight)
 
             #preparing training and testing data
-            target_obs = self.int_to_bool(target[0])
-            target_act = self.bool_to_int(target[1])
-            source_obs = self.int_to_bool(source[0])
-            source_act = self.bool_to_int(source[1])
+            target_obs = target[0]
+            target_act = target[1]
+            source_obs = source[0]
+            source_act = source[1]
             
             #Applying weight to S->Sw
             source_obs = weight * source_obs
-
             #concatenate F and Sw
             if len(w_source)!=0 and len(w_source[0])!=0:
-                print("Appending w_source and source")
-                w_source_obs = self.int_to_bool(w_source[0])
-                w_source_act = self.bool_to_int(w_source[1])
-                source_obs += w_source_obs
-                source_act += w_source_act
+                w_source_obs = np.array(w_source[0][0])
+                w_source_act = np.array(w_source[1])
+                print("w source obs ", w_source_obs.shape)
+                print("source act ", source_obs.shape)
+                source_obs = np.concatenate((source_obs,  w_source_obs))
+                source_act = np.concatenate((source_obs,  w_source_act))
 
             #kFold cross validation
             kf = KFold(n_splits = self.fold)
@@ -182,8 +182,9 @@ class TwoStageTransfer:
                 model.fit(obs_train,act_train)
                 act_predict = model.predict(obs_test)
                 error += 1-accuracy_score(target_act[test], act_predict)
+            
             err.append(error/self.fold)
-        
+            print("Error: ", error)
         max_err_ind = self.__max_val_ind(err)
 
         return weights[max_err_ind]
@@ -205,9 +206,13 @@ class TwoStageTransfer:
         #weights = []
         weight_agent = []
         target_agent_name = list(self.target.keys())[0]
+        self.target[target_agent_name][0] = self.int_to_bool(self.target[target_agent_name][0])
+        self.target[target_agent_name][1] = self.bool_to_int(self.target[target_agent_name][1])
         for agent in self.source:
             print(agent, " in training")
             #phi is an empty set
+            self.source[agent][0] = self.int_to_bool(self.source[agent][0])
+            self.source[agent][1] = self.bool_to_int(self.source[agent][1])
             weight = self.calculate_optimal_weight(self.target[target_agent_name],
                 [],
                 self.source[agent],
@@ -216,10 +221,9 @@ class TwoStageTransfer:
                 [])
 
             weight_agent.append((weight, agent))
-        print(weight_agent)
         sortedS = self.sort_data_by_weight(weight_agent)
         print(sortedS)
-        F = [[],[]]
+        F = [[], []]
         for i in range(self.max_source_dataset):
             weight = self.calculate_optimal_weight(self.target[target_agent_name],
                     F,
@@ -227,16 +231,16 @@ class TwoStageTransfer:
                     self.boosting_iter,
                     self.fold,
                     [])
-            F[0] += list(np.array(self.source[sortedS[i]][0]) * weight)
-            F[1] += self.source[sortedS[i]][1]
-        training_data = [F[0]+self.target[target_agent_name][0],
-                F[1]+self.target[target_agent_name][1]]
+            F[0].append(list(weight * self.source[sortedS[i]][0]))
+            F[1].append(list(self.source[sortedS[i]][1]))
+        training_data = [np.concatenate((F[0],self.target[target_agent_name][0])),
+                np.concatenate((F[1],self.target[target_agent_name][1]))]
 
         return training_data
 
 
     # train decision tree with data of prev games using scikitlearn lib
-    def fit(self):
+    def train(self):
         training_data = self.first_stage()
 
         obs = training_data[0]
@@ -258,7 +262,7 @@ def main():
     print("*                 LOADING DATA                   *")
     print("**************************************************")
 
-    data_loader = DataLoader("./data/agent_data")
+    data_loader = DataLoader("/data1/shared/agent_data/")
     data_loader.load_target_source_data()
     '''
     DATA FORMAT:
@@ -275,13 +279,14 @@ def main():
     print("**************************************************")
     print("*                 TRAINING                       *")
     print("**************************************************")
-    
+    model = Classifier(classname="weka.classifiers.trees.REPTree")
     classifier = TwoStageTransfer(target,
             source,
             boosting_iter=10,
             fold=10,
-            max_source_dataset=len(source))
-    model = classifier.fit()
+            max_source_dataset=len(source),
+            model = model)
+    model = classifier.train()
 
 if __name__== '__main__':
     main()
