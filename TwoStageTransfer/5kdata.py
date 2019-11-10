318e761cef5179e2f8095a24479712feca5140ff
huyenpham2995 <thanhhuyen2995@gmail.com>
2019-11-09 21:54:21 -0800

    included flawed in rulebased data

--- TwoStageTransfer/5kdata.py
+++ TwoStageTransfer/5kdata.py
@@ -5,9 +5,9 @@ from DataLoader import DataLoader
 
 def main():
    data_loader = DataLoader(datapath = "/data1/shared/agent_data/",
-           arff_data_path = "raw/",
+           arff_data_path = "rulebased_data/",
            target_name = "",
-           num_gamese = 1000
+           num_games = 1000
            )
    data_loader.all_agents_datadir = [name for name in os.listdir(data_loader.datapath)]
    data_loader.get_source_data()
